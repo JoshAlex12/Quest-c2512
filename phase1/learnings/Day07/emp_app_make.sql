d13a6f63a86b11451669b85da3c16eab16bef894
Josh Alex <joshdannyalex@gmail.com>
2024-11-14 14:17:19 +0530

    db initialization

--- phase1/learnings/Day07/emp_app_make.sql
+++ phase1/learnings/Day07/emp_app_make.sql
@@ -1,10 +1,11 @@
 -- Install vsc extensiion: "Oracle Developer Tools for VS Code"
 -- $ sqlplus system/4321@localhost:1521/XEPDB1 as sysdba
 -- sql> 
-CREATE USER emp_db IDENTIFIED BY 4321;
+create user emp_db identified by josh;
 
-GRANT CONNECT, RESOURCE TO emp_db;
+grant connect,resource to emp_db;
 
-ALTER USER emp_db QUOTA UNLIMITED ON USERS;
+alter user emp_db
+   quota unlimited on users;
 
 -- -CONNECT emp_db/4321@localhost:1521/XEPDB1;
